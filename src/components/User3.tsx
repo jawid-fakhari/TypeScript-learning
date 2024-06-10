fe87e273ad3f771bdb62c834d6771da9b6dcc659
Jawid Fakhari <jawid_fakhari@live.it>
2024-06-10 15:02:51 +0200

    interface in typescript

--- src/components/User3.tsx
+++ src/components/User3.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
+// esempio in interface IUser, interfaces in typescript, qunado vogliamo usare on object possiamo usare interface
+interface IUser {
+    //qui possiamo dichiarare il type come string o any perché nello state abbiamo gia dichiarato string
+    value: string;
+    //il type di setState è diverso, dobbiamo prenderlo direttamente dall'react facendo hover sul setState e cosi react ci da il type che possiamo copiare e incollare
+    onChange: React.Dispatch<React.SetStateAction<string>>;
+}
+
+// esempio in type TUser
 
 type TUser = {
     //qui possiamo dichiarare il type come string o any perché nello state abbiamo gia dichiarato string
     value: string;
     //il type di setState è diverso, dobbiamo prenderlo direttamente dall'react facendo hover sul setState e cosi react ci da il type che possiamo copiare e incollare
-    onChange: React.Dispatch<React.SetStateAction<string>>; 
+    onChange: React.Dispatch<React.SetStateAction<string>>;
 }
 function User3({value, onChange}: TUser) {
   return (
