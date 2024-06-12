94e4c7fe92e99708ef6103742bad33582426a495
Jawid Fakhari <jawid_fakhari@live.it>
2024-06-12 16:47:24 +0200

    uso di Omit

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -15,6 +15,9 @@ type USER={
     mother: MotherType
 }
 
+// Omit cosa è? se vogliamo creare un type che è uguale a quello precedente ma un type in meno allora usiamo Omit 
+type User2 = Omit<USER, "mother" | "brother">; // in questo caso User2 inherita tutto da USER tranne che mother e brother
+
 //come settare il type dei props in react component, che è stato importato in App.tsx
 export default function User({name, age, hasChild, minus, father, brother, mother}: USER,) {
   return (
